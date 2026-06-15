37e50b2b2adcc67de1bf41312af34752897de795
wintony <winton2095@gmail.com>
2026-06-15 18:56:38 -0400

    Refactor display board logic

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -3,25 +3,41 @@ GAME_STATE = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 WINNING_MOVES = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def start_game():
-    print("Welcome to Tic Tac Toe!\n")
+    print("Welcome to Tic Tac Toe!")
+
+def display_board():
+    print("\nCurrent game board:\n")
+
+    for i in range(0, 9, 3):
+        row = ""
+        for j in range(i, i+3):
+            if GAME_STATE[j] == " ":
+                row += " "
+            else:
+                row += GAME_STATE[j]
+            if j < i+2:
+                row += "|"
+        print(row)
+        if i < 6:
+            print("-----")
+
+    # TODO: Use range to condense the below logic
+    # print(f"{GAME_STATE[0]}|{GAME_STATE[1]}|{GAME_STATE[2]}")
+    # print("-----")
+    # print(f"{GAME_STATE[3]}|{GAME_STATE[4]}|{GAME_STATE[5]}")
+    # print("-----")
+    # print(f"{GAME_STATE[6]}|{GAME_STATE[7]}|{GAME_STATE[8]}\n")
 
 def display_current_game_state():
     if CURRENT_PLAYER == 1:
-        print("It is Player One's (X) turn.\n")
+        print("\nIt is Player One's (X) turn.")
     elif CURRENT_PLAYER == 2:
-        print("It is Player Two's (O) turn.\n")
+        print("\nIt is Player Two's (O) turn.")
 
-    print("Current game board:\n")
-
-    # TODO: Use range to condense the below logic
-    print(f"{GAME_STATE[0]}|{GAME_STATE[1]}|{GAME_STATE[2]}")
-    print("-----")
-    print(f"{GAME_STATE[3]}|{GAME_STATE[4]}|{GAME_STATE[5]}")
-    print("-----")
-    print(f"{GAME_STATE[6]}|{GAME_STATE[7]}|{GAME_STATE[8]}\n")
+    display_board()
 
 def take_user_move():
-    print("Available moves are shown below:\n")
+    print("\nAvailable moves are shown below:\n")
 
     for i in range(0, 9, 3):
         row = ""
@@ -42,7 +58,7 @@ def take_user_move():
     move = None
 
     while not is_valid_user_input:
-        user_input = input("Please enter the number where you would like to place your next move: ")
+        user_input = input("\nPlease enter the number where you would like to place your next move: ")
 
         try:
             move = int(user_input)
@@ -68,17 +84,19 @@ def check_game_ongoing():
             move_available = True
 
     if not move_available:
-        print("Player One and Player Two have tied!")
+        display_board()
+        print("\nPlayer One and Player Two have tied!")
         return False
 
     # TODO: only check winning moves containing the most recent user input move
     for winning_move in WINNING_MOVES:
         # TODO: Improve logic check to make sure "winning move" is not 3-in-a-row of blank spaces
         if (GAME_STATE[winning_move[0]] == GAME_STATE[winning_move[1]] == GAME_STATE[winning_move[2]]) and (GAME_STATE[winning_move[0]] != " "):
+            display_board()
             if CURRENT_PLAYER == 1:
-                print("Player One (X) wins!")
+                print("\nPlayer One (X) wins!")
             elif CURRENT_PLAYER == 2:
-                print("Player Two (O) wins!")
+                print("\nPlayer Two (O) wins!")
             return False
 
     return True
