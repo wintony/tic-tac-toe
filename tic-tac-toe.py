60255889a49e1aeec7dffbc528bf3876055a675c
wintony <winton2095@gmail.com>
2026-06-15 16:37:03 -0400

    Add basic playable game flow between two players

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,5 +1,6 @@
 CURRENT_PLAYER = 1
 GAME_STATE = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+WINNING_MOVES = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def start_game():
     print("Welcome to Tic Tac Toe!\n")
@@ -7,11 +8,12 @@ def start_game():
 def display_current_game_state():
     if CURRENT_PLAYER == 1:
         print("It is Player One's (X) turn.\n")
-    if CURRENT_PLAYER == 2:
+    elif CURRENT_PLAYER == 2:
         print("It is Player Two's (O) turn.\n")
 
     print("Current game board:\n")
 
+    # TODO: Use range to condense the below logic
     print(f"{GAME_STATE[0]}|{GAME_STATE[1]}|{GAME_STATE[2]}")
     print("-----")
     print(f"{GAME_STATE[3]}|{GAME_STATE[4]}|{GAME_STATE[5]}")
@@ -26,18 +28,62 @@ def take_user_move():
         for j in range(i, i+3):
             if GAME_STATE[j] == " ":
                 row += str(j+1)
+            else:
+                row += "#"
             if j < i+2:
                 row += "|"
         print(row)
         if i < 6:
             print("-----")
 
-    move = input("Please enter the number where you would like to place your next move: ")
+    # TODO: Validate input is a valid move int
+    move = int(input("Please enter the number where you would like to place your next move: "))
+
+    if CURRENT_PLAYER == 1:
+        GAME_STATE[move-1] = "X"
+    elif CURRENT_PLAYER == 2:
+        GAME_STATE[move-1] = "O"
+
+def check_game_ongoing():
+    # Check if any moves are still available
+    # TODO: Keep track of user moves. If move == 9, return True
+    move_available = False
+
+    for i in range(9):
+        if GAME_STATE[i] == " ":
+            move_available = True
+
+    if not move_available:
+        print("No moves available")
+        return False
+
+    # TODO: only check winning moves containing the most recent user input move
+    for winning_move in WINNING_MOVES:
+        # TODO: Improve logic check to make sure "winning move" is not 3-in-a-row of blank spaces
+        if (GAME_STATE[winning_move[0]] == GAME_STATE[winning_move[1]] == GAME_STATE[winning_move[2]]) and (GAME_STATE[winning_move[0]] != " "):
+            print(f"Winning move: {winning_move}" )
+            return False
+
+    return True
+
+def switch_current_player():
+    global CURRENT_PLAYER
+
+    if CURRENT_PLAYER == 1:
+        CURRENT_PLAYER = 2
+    elif CURRENT_PLAYER == 2:
+        CURRENT_PLAYER = 1
 
 def main():
     start_game()
-    display_current_game_state()
-    take_user_move()
+
+    is_game_ongoing = True
+
+    while is_game_ongoing:
+        display_current_game_state()
+        take_user_move()
+        is_game_ongoing = check_game_ongoing()
+        switch_current_player()
 
 if __name__ == "__main__":
     main()
